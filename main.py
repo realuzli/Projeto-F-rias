2f9ebe053f60c1fbd43c16ab6d6d5b4335d5f3c5
realuzli <luizvazvneto@gmail.com>
2026-07-16 01:05:35 -0300

    Implementação de algumas funções e correções,correções

--- main.py
+++ main.py
@@ -6,54 +6,42 @@ import os
 def limpar_tela():
     os.system('cls' if os.name == 'nt' else 'clear')
 def registrar_treino():
-    dicionarioRegistroTreino = {}
-
+    dicionario_registro_treino = {}
+    tipos_treino = []
     print("\nBem-vindo, vamos registrar o seu treino!")
     print("\nPrecisamos que você responda algumas perguntas antes de registrarmos o seu treino!")
 
     while True:
         try:
-            qtdTreinosSemana = int(input("Quantos dias você treina na semana? "))
+            qtd_treinos_semana = int(input("Quantos dias você treina na semana? "))
             break
         except ValueError:
             print("\nPor favor, digite um numero inteiro.")
 
-    tiposTreino = []
-    dicionarioExercicio = {}
 
-    for _ in range(qtdTreinosSemana):
-        nomeTipoTreino = str(input((f"Diga o nome da sua divisão de treino do dia {_ + 1}: ")))
-        tiposTreino.append(nomeTipoTreino)
-        dicionarioRegistroTreino[nomeTipoTreino] = {}
+    for _ in range(qtd_treinos_semana):
+        nome_tipo_treino = str(input((f"Diga o nome da sua divisão de treino do dia {_ + 1}: ")))
+        tipos_treino.append(nome_tipo_treino)
+        dicionario_registro_treino[nome_tipo_treino] = {}
 
-    for _ in range(qtdTreinosSemana):
-        print(f"Dia {_+1} -> {tiposTreino[_]}")
+    for _ in range(qtd_treinos_semana):
+        print(f"Dia {_+1} -> {tipos_treino[_]}")
 
-    while True:
-        try:
-            qtdExerciciosTreino = int(input("Quantos exercícios em média por tipo de treino? "))
-            break
-        except ValueError:
-             print("\nPor favor, digite um numero inteiro.")
+    for _ in range(len(tipos_treino)):
+        quantidade = int (input(f"Quantos exercícios no dia de {tipos_treino[_]}? "))
+        for i in range(quantidade):
+            nome_exercicio = str(input("Nome do exercício: "))
+            dicionario_registro_treino[tipos_treino[_]][nome_exercicio] = {"series":[],"carga":[],"reps":[]}
+            print(f"Exercício {nome_exercicio} adicionado com sucesso no dia de {tipos_treino[_]}!")
 
-    for i in range(qtdTreinosSemana):
-        print(f"\nVamos adicionar os exercícios do dia {tiposTreino[i]}")
-        dicionarioExercicio[tiposTreino[i]] = []
-
-        for _ in range(qtdExerciciosTreino):
-            exercicio = str(input((f"Exercício {_ +1}: ")))
-            
-            dicionarioRegistroTreino[tiposTreino[i]][exercicio] = {"series":[],"reps":[],"carga":[]}        
-            dicionarioExercicio[tiposTreino[i]].append(exercicio)
-           
     
   
-    return dicionarioRegistroTreino,dicionarioExercicio,tiposTreino
+    return dicionario_registro_treino,tipos_treino
 
 
-def adicionar_treino(tipo_de_treino, dicionario_treino,exercicios_treino):
+def adicionar_treino(tipo_de_treino, dicionario_treino):
     while True:
-        listar_Treino(tipo_de_treino,exercicios_treino)
+        listar_treino(tipo_de_treino,dicionario_treino)
         exercicio_adicionado = str(input("Qual o exercício adicionado? "))
         if exercicio_adicionado in dicionario_treino[tipo_de_treino]:
             break
@@ -65,17 +53,12 @@ def adicionar_treino(tipo_de_treino, dicionario_treino,exercicios_treino):
     for i in range(quantidade_series):
         carga = float(input((f"Carga da {i+1}° série: ")))
         reps = int(input((f"Repetições da {i+1}° série: ")))
-        dicionario_treino[tipo_de_treino][exercicio_adicionado]["series"].append(i)
+        dicionario_treino[tipo_de_treino][exercicio_adicionado]["series"].append(i+1)
         dicionario_treino[tipo_de_treino][exercicio_adicionado]["carga"].append(carga)
         dicionario_treino[tipo_de_treino][exercicio_adicionado]["reps"].append(reps)
-    listar_Treino(tipo_de_treino,dicionario_treino)
-
-
-
-
-
+    listar_treino(tipo_de_treino,dicionario_treino)
     
-def listar_Treino(tipo_de_treino,exercicios_treino):
+def listar_treino(tipo_de_treino,dicionario_treino):
     limpar_tela()
     print("Escolha uma das opções de tipos de treino para listar: ")
         
@@ -85,23 +68,19 @@ def listar_Treino(tipo_de_treino,exercicios_treino):
     escolhido = int(input("Escolhido: "))
     
     
-    for key,value in exercicios_treino.items():
+    for key,value in dicionario_treino.values():
         if key==tipo_de_treino[escolhido-1]:
             print(f"Treino {key} | Exercícios -> {value}")
             
-
-
-def consultar_Treino(tipo_de_treino,dicionario_treino):
+def consultar_treino(tipo_de_treino,dicionario_treino):
     limpar_tela()
-    if KeyError:
-        print(f"O treino ainda não foi adicionado.")
-        print(f"Tente adicionar um treino para poder consultar.")
-    else:
-        for key,value in dicionario_treino[tipo_de_treino].items():
+    try:
+        for key, value in dicionario_treino[tipo_de_treino].items():
             print(f"Treino {key} | Exercícios -> {value}")
+    except KeyError:
+        print("O treino ainda não foi adicionado.")
 
 def main():
-    registro_exercicios_do_treino ={}
     registro_do_treino = {}
     tipos_de_treino = []
     while True:
@@ -120,8 +99,8 @@ def main():
             print("\nVocê escolheu a opção de registrar um treino!")
             print("\nVamos começar!")
             
-            registro_do_treino,registro_exercicios_do_treino,tipos_de_treino = registrar_treino()
-            tipos_de_treino = list(registro_exercicios_do_treino.keys())            
+            registro_do_treino,tipos_de_treino = registrar_treino()
+            tipos_de_treino = list(registro_do_treino.keys())            
             print("Seu treino foi registrado com sucesso.")
         
         elif opcao == 2:
@@ -133,8 +112,8 @@ def main():
                 for i in range(len(tipos_de_treino)):
                     print(f"Opção {i+1} - {tipos_de_treino[i]}")
 
-                    escolhido = int(input("Escolhido: "))
-                adicionar_treino(tipos_de_treino[escolhido-1],registro_do_treino,registro_exercicios_do_treino)
+                escolhido = int(input("Escolhido: "))
+                adicionar_treino(tipos_de_treino[escolhido-1],registro_do_treino)
                 
         
         elif opcao== 3: 
@@ -150,14 +129,14 @@ def main():
                     print(f"{i+1} - {tipos_de_treino[i]}")
 
                 escolhido = int(input("Escolhido: "))
-                consultar_Treino(tipos_de_treino[escolhido-1],registro_do_treino)
+                consultar_treino(tipos_de_treino[escolhido-1],registro_do_treino)
         
         elif opcao== 4:
             if not registro_do_treino:
                 print("Nenhum treino registrado no momento.")
             
             else:
-                listar_Treino(tipos_de_treino,registro_exercicios_do_treino)
+                listar_treino(tipos_de_treino,registro_do_treino)
                 
         elif opcao== 0:
             print("\nVocê escolheu a opção de encerrar o progama!")
