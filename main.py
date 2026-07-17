b49045ae667260c0664ebaf52f83e57dfa765b84
realuzli <luizvazvneto@gmail.com>
2026-07-16 21:45:10 -0300

    Atualização do main.py e readme.md

--- main.py
+++ main.py
@@ -1,150 +1,147 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pylab as mlb
-import datetime as dt
 import os
 def limpar_tela():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('cls' if os.name== 'nt' else 'clear')
+
 def registrar_treino():
-    dicionario_registro_treino = {}
+    registro_de_treino = {}
+    exercicios_registrados = {}
     tipos_treino = []
-    print("\nBem-vindo, vamos registrar o seu treino!")
-    print("\nPrecisamos que você responda algumas perguntas antes de registrarmos o seu treino!")
+
+    print(f"Vamos começar o registro do seu treino!")
+    print(f"Precisamos que você responda algumas perguntas antes de começarmos!")
 
     while True:
         try:
-            qtd_treinos_semana = int(input("Quantos dias você treina na semana? "))
+            quantidade_de_tipos_de_treino_diferentes = int(input("Quantos dias possui a sua divisão de treino? "))
             break
         except ValueError:
-            print("\nPor favor, digite um numero inteiro.")
+            print("Por favor, digite um numero inteiro.")
+    limpar_tela()
 
-
-    for _ in range(qtd_treinos_semana):
-        nome_tipo_treino = str(input((f"Diga o nome da sua divisão de treino do dia {_ + 1}: ")))
+    for dia in range(quantidade_de_tipos_de_treino_diferentes):
+        nome_tipo_treino = str(input(f"Diga o {dia+1}° nome da sua divisão de treino: ")).upper()
         tipos_treino.append(nome_tipo_treino)
-        dicionario_registro_treino[nome_tipo_treino] = {}
+        registro_de_treino[nome_tipo_treino] = {}
 
-    for _ in range(qtd_treinos_semana):
-        print(f"Dia {_+1} -> {tipos_treino[_]}")
+    for diaS in range(quantidade_de_tipos_de_treino_diferentes):
+        while True:
+            try:
+                limpar_tela()
+                quantidade_exercicios = int(input(f"Quantos exercícios no dia de {tipos_treino[diaS]}? "))
+                exercicios_registrados[tipos_treino[diaS]] = []
+                break
+            except ValueError:
+                print("Digite um numero inteiro.")
+        for nomes in range(quantidade_exercicios):
+            nome_exercicio = str(input("Nome do exercício: ")).upper()
+            registro_de_treino[tipos_treino[diaS]][nome_exercicio] = []
+            exercicios_registrados[tipos_treino[diaS]].append(nome_exercicio)
+            print(f"Exercício {nome_exercicio} adicionado com sucesso no dia de {tipos_treino[diaS]}!")
 
-    for _ in range(len(tipos_treino)):
-        quantidade = int (input(f"Quantos exercícios no dia de {tipos_treino[_]}? "))
-        for i in range(quantidade):
-            nome_exercicio = str(input("Nome do exercício: "))
-            dicionario_registro_treino[tipos_treino[_]][nome_exercicio] = {"series":[],"carga":[],"reps":[]}
-            print(f"Exercício {nome_exercicio} adicionado com sucesso no dia de {tipos_treino[_]}!")
-
-    
+    pausar_execucao()
+    return registro_de_treino,exercicios_registrados,tipos_treino
   
-    return dicionario_registro_treino,tipos_treino
+def adicionar_treino(registro_de_treino,tipos_de_treino,exercicios_registrados):
+    tipo_de_treino_escolhido = escolher_tipo_de_treino(tipos_de_treino)
+    exercicio_escolhido = escolher_exercicio(tipo_de_treino_escolhido,exercicios_registrados)
+    print(f"Exercício escolhido -> {exercicio_escolhido} | Tipo de treino do exercício escolhido -> {tipo_de_treino_escolhido}")
 
+    quantidade_series = int(input("Quantidade de séries desse exercício?"))
 
-def adicionar_treino(tipo_de_treino, dicionario_treino):
+    for quantidade in range(quantidade_series):
+        carga = float(input(f"Carga da {quantidade+1}° série: "))
+        reps = int(input(f"Repetições da {quantidade+1}° série: "))
+        registro_de_treino[tipo_de_treino_escolhido][exercicio_escolhido].append({"série":quantidade+1,"carga":carga,"reps":reps})
+
+    print(f"Treino de {exercicio_escolhido} salvo com sucesso: {registro_de_treino[tipo_de_treino_escolhido][exercicio_escolhido]}")
+    pausar_execucao()
+
+def pausar_execucao():
+    input("Pressione 'Enter' para continuar: ")
+
+def mostrar_tipos_de_treino(tipos_de_treino):
+    for posicao,nome_do_tipo in enumerate(tipos_de_treino):
+        print(f"{posicao} -> {nome_do_tipo}")
+
+def escolher_tipo_de_treino(tipos_de_treino):
     while True:
-        listar_treino(tipo_de_treino,dicionario_treino)
-        exercicio_adicionado = str(input("Qual o exercício adicionado? "))
-        if exercicio_adicionado in dicionario_treino[tipo_de_treino]:
-            break
+        limpar_tela()
+        mostrar_tipos_de_treino(tipos_de_treino)
+        tipo_de_treino_escolhido = str(input("Digite o nome do tipo de treino escolhido: ")).upper()
+        if tipo_de_treino_escolhido not in tipos_de_treino:
+            print("Digite um tipo de treino existente.")
+       
         else:
-            print("Digite o nome de um exercício registrado.")
+            print(f"Você escolheu [{tipo_de_treino_escolhido}]")
+            return tipo_de_treino_escolhido
 
-    quantidade_series = int(input("Quantidade de séries? "))
-
-    for i in range(quantidade_series):
-        carga = float(input((f"Carga da {i+1}° série: ")))
-        reps = int(input((f"Repetições da {i+1}° série: ")))
-        dicionario_treino[tipo_de_treino][exercicio_adicionado]["series"].append(i+1)
-        dicionario_treino[tipo_de_treino][exercicio_adicionado]["carga"].append(carga)
-        dicionario_treino[tipo_de_treino][exercicio_adicionado]["reps"].append(reps)
-    listar_treino(tipo_de_treino,dicionario_treino)
-    
-def listar_treino(tipo_de_treino,dicionario_treino):
-    limpar_tela()
-    print("Escolha uma das opções de tipos de treino para listar: ")
-        
-    for i in range(len(tipo_de_treino)):
-        print(f"Opção {i+1} - {tipo_de_treino[i]}")
-
-    escolhido = int(input("Escolhido: "))
-    
-    
-    for key,value in dicionario_treino.values():
-        if key==tipo_de_treino[escolhido-1]:
-            print(f"Treino {key} | Exercícios -> {value}")
-            
-def consultar_treino(tipo_de_treino,dicionario_treino):
-    limpar_tela()
-    try:
-        for key, value in dicionario_treino[tipo_de_treino].items():
-            print(f"Treino {key} | Exercícios -> {value}")
-    except KeyError:
-        print("O treino ainda não foi adicionado.")
-
+def mostrar_exercicios_registrados(tipo_de_treino_escolhido,exercicios_registrados):
+    for key,value in exercicios_registrados.items():
+        if key == tipo_de_treino_escolhido:
+            print(f"Tipo de treino [{key}] | Exercícios -> [{value}]")
+def escolher_exercicio(tipo_de_treino_escolhido,exercicios_registrados):
+    mostrar_exercicios_registrados(tipo_de_treino_escolhido,exercicios_registrados)
+    exercicio_escolhido = str(input("Exercício escolhido: ")).upper()
+    return exercicio_escolhido
+def mostrar_treinos_registrados(registro_de_treino):
+    for key,value in registro_de_treino.items():
+        print(f"Treino de {key} | Registro -> {value}")    
+    pausar_execucao()
 def main():
-    registro_do_treino = {}
+    exercicios_registrados = {}
     tipos_de_treino = []
+    registro_de_treino = {}
+
     while True:
-        limpar_tela()  
-        print("Escolha o modo de funcionamento do aplicativo:")
+        limpar_tela()
+        print("Escolha o modo de funcionamento do terminal: ")
         print("\nOpções de funcionamento do aplicativo disponíveis:\n",
-        "[1] - Registrar um treino\n",
-        "[2] - Adicionar um treino\n",
+        "[1] - Registrar um treino (criar a divisão dos treinos)\n",
+        "[2] - Adicionar um treino (lançar séries,cargas e repetições)\n",
         "[3] - Consultar os dados de treino\n",
-        "[4] - Listar treino\n",
+        "[4] - Listar exercícios registrados\n",
         "[0] - Sair do progama")
 
-        opcao = int(input("\nOpção de funcionamento escolhida: "))
-
-        if opcao == 1:
-            print("\nVocê escolheu a opção de registrar um treino!")
-            print("\nVamos começar!")
-            
-            registro_do_treino,tipos_de_treino = registrar_treino()
-            tipos_de_treino = list(registro_do_treino.keys())            
-            print("Seu treino foi registrado com sucesso.")
-        
-        elif opcao == 2:
-            if  not registro_do_treino:
-                print("Nenhum treino registrado no momento.")
-            
-            else:
-                print("\nVocê escolheu a opção de adicionar um treino!")
-                for i in range(len(tipos_de_treino)):
-                    print(f"Opção {i+1} - {tipos_de_treino[i]}")
-
-                escolhido = int(input("Escolhido: "))
-                adicionar_treino(tipos_de_treino[escolhido-1],registro_do_treino)
-                
-        
-        elif opcao== 3: 
-            if not registro_do_treino:
-                print(f"Nenhum exercício registrado no momento.")
-                print(f"Tente registrar primeiro.")
-            
-            else:       
-                print("\nVocê escolheu a opção de consultar os dados de treino!")
-                print("Digite o numero de um dos tipos de treino: ")
-        
-                for i in range(len(tipos_de_treino)):
-                    print(f"{i+1} - {tipos_de_treino[i]}")
-
-                escolhido = int(input("Escolhido: "))
-                consultar_treino(tipos_de_treino[escolhido-1],registro_do_treino)
-        
-        elif opcao== 4:
-            if not registro_do_treino:
-                print("Nenhum treino registrado no momento.")
-            
-            else:
-                listar_treino(tipos_de_treino,registro_do_treino)
-                
-        elif opcao== 0:
-            print("\nVocê escolheu a opção de encerrar o progama!")
-            print("Até breve!")
-            break
-        
-        else:
+        try:
+            opcao_escolhida = int(input("\nOpção escolhida: "))
+        except ValueError:
             print("Digite uma opção válida.")
+            pausar_execucao()
+            continue
         
+        match opcao_escolhida:
+            
+            case 0:
+                limpar_tela()
+                print("Você escolheu a opção de encerrar o progama!")
+                print("Até breve!")
+                break
+            case 1:
+                registro_de_treino,exercicios_registrados,tipos_de_treino = registrar_treino()
+            
+            case 2:
+                if not registro_de_treino:
+                    print(f"Nenhum treino registrado.")
+                    print(f"Tente registrar um treino primeiro.")
+                else:
+                    adicionar_treino(registro_de_treino,tipos_de_treino,exercicios_registrados)
+            
+            case 3:
+                if not registro_de_treino:
+                    print(f"Nenhum treino registrado.")
+                    print(f"Tente registrar um treino primeiro.")
+                else:
+                    mostrar_treinos_registrados(registro_de_treino)
+            
+            case 4:
+                if not registro_de_treino:
+                    print(f"Nenhum treino registrado.")
+                    print(f"Tente registrar um treino primeiro.")
+                else:
+                    tipo_de_treino_escolhido =escolher_tipo_de_treino(tipos_de_treino)
+                    mostrar_exercicios_registrados(tipo_de_treino_escolhido,exercicios_registrados)
+            
+          
+
 if __name__ == "__main__":
     main()
